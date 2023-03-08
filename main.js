455ecc97740aecfe8ee9364608261a5adec8c305
Ihor Pronoza <106109790+Ipronoza@users.noreply.github.com>
2023-03-08 23:16:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -112,17 +112,17 @@ fetch(urlForDay)
 .then(data => {
 
 
-    for(i = 1; i<4; i++){
+    for(let i = 1; i<4; i++){
         document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°C";
     }
 
-    for(i = 1; i<4; i++){
+    for(let i = 1; i<4; i++){
         document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(1) + "°C";
     }
     //------------------------------------------------------------
 
     //Getting Weather Icons
-     for(i = 1; i<4; i++){
+     for(let i = 1; i<4; i++){
         document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
         data.list[i].weather[0].icon
         +".png";
@@ -146,7 +146,7 @@ let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thuesday', 'Friday', 'S
 let month = date.getMonth()
 let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 let year = date.getFullYear();
-weatherDate.innerHTML = `${days[day]} ${months[month - 1]} ${year}`;
+weatherDate.innerHTML = `${days[day]} ${months[month]} ${year}`;
 
 //anim text
 // London
@@ -185,6 +185,6 @@ function CheckDay(day){
     }
 }
 
-    for(i = 1; i<4; i++){
+    for(let i = 1; i<4; i++){
         document.getElementById("day" + (i+1)).innerHTML = days[CheckDay(i)];
     }
